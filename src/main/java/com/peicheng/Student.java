d9ecc78be002e2d760cf150a47a2df311e127121
Pei Cheng Huang <peicheng@vt.edu>
2023-02-06 13:43:42 -0800

    create db and schema

---
+++ src/main/java/com/peicheng/Student.java
@@ -0,0 +1,91 @@
+package com.peicheng;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_id_sequence",
+            sequenceName = "student_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Student(Integer id,
+                   String name,
+                   String email,
+                   Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/peicheng/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.peicheng;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    //TODO: CRUD
+}
+
